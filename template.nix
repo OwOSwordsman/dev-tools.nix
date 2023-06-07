58a26ecf6901dc4d30e25345708ead15fb830236
OwO Swordsman <owoswordsman@gmail.com>
2023-06-07 15:30:36 -0400

    Initial commit

---
+++ template.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        packages.default = pkgs.stdenv.mkDerivation rec {
+          pname = "jdt-language-server";
+          version = "{{version}}";
+          timestamp = "{{timestamp}}";
+
+          src = pkgs.fetchurl {
+            url = "https://download.eclipse.org/jdtls/milestones/${version}/jdt-language-server-${version}-${timestamp}.tar.gz";
+            sha256 = "{{hash}}";
+          };
+
+          nativeBuildInputs = with pkgs; [makeWrapper];
+
+          buildPhase = ''
+            mkdir -p jdt-language-server
+            tar xfz $src -C jdt-language-server
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin $out/libexec
+            cp -a jdt-language-server $out/libexec
+            makeWrapper $out/libexec/jdt-language-server/bin/jdtls $out/bin/jdtls \
+              --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.python3Minimal]}
+          '';
+
+          dontUnpack = true;
+          dontPatch = true;
+          dontConfigure = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [python3Minimal pyright];
+        };
+      }
+    );
+}
