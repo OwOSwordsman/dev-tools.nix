8ba222ea0389bfb4ba63f725235871da1a0c0ed1
Github <actions@github.com>
2023-07-01 19:41:53 +0000

    chore: update packages

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         };
       in {
         packages.jdtls = pkgs.stdenv.mkDerivation rec {
-          pname = "jdt-language-server";
+          pname = "jdtls";
           version = "1.25.0";
           timestamp = "202306291518";
 
@@ -36,7 +36,31 @@
             mkdir -p $out/bin $out/libexec
             cp -a jdt-language-server $out/libexec
             makeWrapper $out/libexec/jdt-language-server/bin/jdtls $out/bin/jdtls \
-              --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.jdk pkgs.python3Minimal]}
+              --set PATH ${pkgs.lib.makeBinPath [pkgs.jdk pkgs.python3Minimal]}
+          '';
+
+          dontUnpack = true;
+          dontPatch = true;
+          dontConfigure = true;
+        };
+
+        packages.junit = pkgs.stdenv.mkDerivation rec {
+          pname = "junit";
+          version = "1.9.3";
+
+          jar = pkgs.fetchurl {
+            url = "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/${version}/junit-platform-console-standalone-${version}.jar";
+            sha512 = "sha512-v60+M9VpyUrJhVGlXg7rdKg2FJT8jGomzQzdR7sWBOI+R/sFjjYqlTqNUa3W2hTrDRK3tuzFKgWmLW1KHQbvjA==";
+          };
+
+          nativeBuildInputs = with pkgs; [makeWrapper];
+
+          installPhase = ''
+            mkdir -p $out/bin $out/libexec/junit
+            cp ${jar} $out/libexec/junit/junit-platform-console-standalone.jar
+            makeWrapper ${pkgs.jdk}/bin/java $out/bin/junit \
+              --add-flags "-jar" \
+              --add-flags "$out/libexec/junit/junit-platform-console-standalone.jar"
           '';
 
           dontUnpack = true;
@@ -45,7 +69,7 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [python3];
+          packages = with pkgs; [python3 python3Packages.icecream];
         };
       }
     );
