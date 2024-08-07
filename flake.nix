840848b551a5a8327b6528505388f9d5fd0d6f61
Alvin Shen <29553973+alven-shin@users.noreply.github.com>
2024-08-06 21:38:04 -0400

    feat: reset project

--- flake.nix
+++ flake.nix
@@ -1,76 +1,37 @@
 {
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in {
-        packages.jdtls = pkgs.stdenv.mkDerivation rec {
-          pname = "jdtls";
-          version = "1.38.0";
-          timestamp = "202408011337";
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      # debug = true;
 
-          src = pkgs.fetchurl {
-            url = "https://download.eclipse.org/jdtls/milestones/${version}/jdt-language-server-${version}-${timestamp}.tar.gz";
-            sha256 = "sha256-uml3iKGfK6V7FjAqums0PGSZKMlfdrDRcElKwS0XrHg=";
-          };
-
-          nativeBuildInputs = with pkgs; [makeWrapper];
-
-          buildPhase = ''
-            mkdir -p jdt-language-server
-            tar xfz $src -C jdt-language-server
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin $out/libexec
-            cp -a jdt-language-server $out/libexec
-            makeWrapper $out/libexec/jdt-language-server/bin/jdtls $out/bin/jdtls \
-              --set PATH ${pkgs.lib.makeBinPath [pkgs.jdk17_headless pkgs.python3Minimal]}
-          '';
-
-          dontUnpack = true;
-          dontPatch = true;
-          dontConfigure = true;
-        };
-
-        packages.junit = pkgs.stdenv.mkDerivation rec {
-          pname = "junit";
-          version = "1.10.3";
-
-          jar = pkgs.fetchurl {
-            url = "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/${version}/junit-platform-console-standalone-${version}.jar";
-            sha512 = "sha512-jWwsKrVaY3YG249by1owhv5wdcua/nQHo4zXjVwMoU7hUm3tAA6VFqBpdm+IrHHeszwOnffmdG5KkgBDENSVkg==";
-          };
-
-          nativeBuildInputs = with pkgs; [makeWrapper];
-
-          installPhase = ''
-            mkdir -p $out/bin $out/libexec/junit
-            cp ${jar} $out/libexec/junit/junit-platform-console-standalone.jar
-            makeWrapper ${pkgs.jdk17_headless}/bin/java $out/bin/junit \
-              --add-flags "-jar" \
-              --add-flags "$out/libexec/junit/junit-platform-console-standalone.jar"
-          '';
-
-          dontUnpack = true;
-          dontPatch = true;
-          dontConfigure = true;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [python3 python3Packages.icecream];
-        };
-      }
-    );
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+        ./nix/shell.nix
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
 }
